ebd5d3f6ba2cd1424d25ecd78af3a4db718391f8
LIM GEN JACK <135495371+Jack-1118@users.noreply.github.com>
2024-03-08 09:45:08 +0800

    heroSection

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Navbar from "./Components/Navbar";
+import HeroSection from "./Components/HeroSection";
 
 function App() {
   return (
     <React.Fragment>
-      <Navbar></Navbar>
+      <Navbar />
+      <HeroSection />
     </React.Fragment>
   );
 }
