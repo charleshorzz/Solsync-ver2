beee21395b17d5eba04bd92ec139572d879afcda
CHONG KELVIN <111684233+Kelocker@users.noreply.github.com>
2024-03-10 22:18:30 +0800

    forum initial push

---
+++ src/Components/PostRender.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../Styles/contentRender.css'; // Ensure to create a CSS file with this name
+
+function SocialCard() {
+  return (
+    <div>
+      Hello
+    </div>
+  );
+}
+
+export default SocialCard;
